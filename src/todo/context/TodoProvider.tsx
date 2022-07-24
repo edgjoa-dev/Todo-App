8d59231428373f9d553d2474f5bb74d363327654
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-07-23 21:38:09 -0500

    Creacion de todoReducer e implementación en provider

--- src/todo/context/TodoProvider.tsx
+++ src/todo/context/TodoProvider.tsx
@@ -1,13 +1,15 @@
 import { TodoContext } from "./TodoContext"
 import { TodoState } from '../interfaces/interfaces';
+import { useReducer } from "react";
+import { TodoReducer } from "./TodoReducer";
 
     const INITIAL_STATE: TodoState = {
         todoCount: 2,
         todos: [
-            { id: 1, text: "todo 1", completed: false },
-            { id: 2, text: "todo 2", completed: false },
-            { id: 3, text: "todo 3", completed: false },
-            { id: 4, text: "todo 4", completed: false },
+            { id: '1', desc: "todo 1", completed: false },
+            { id: '2', desc: "todo 2", completed: false },
+            { id: '3', desc: "todo 3", completed: false },
+            { id: '4', desc: "todo 4", completed: false },
         ],
         completed: 0,
         pending: 2,
@@ -18,8 +20,11 @@ interface TodoProps {
 }
 
 export const TodoProvider = ({children}: TodoProps ) => {
+
+    const [ todoState, dispatch ] = useReducer( TodoReducer, INITIAL_STATE )
+
     return (
-        <TodoContext.Provider value={{}} >
+        <TodoContext.Provider value={{ todoState }} >
             { children }
         </TodoContext.Provider>
     )
