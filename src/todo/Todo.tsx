e8607dc2a64c7fff99be04fbe281e40605301114
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-07-23 21:08:32 -0500

    Estructuración de aplocacion

---
+++ src/todo/Todo.tsx
@@ -0,0 +1,14 @@
+
+export const Todo = () => {
+    return (
+        <div>
+            <h1>Todo</h1>
+            <ul>
+                <li>todo 1</li>
+                <li>todo 2</li>
+                <li>todo 3</li>
+                <li>todo 4</li>
+            </ul>
+        </div>
+    )
+}
