c5906dd5010324bc6bfc7c368641a045e1d97bd4
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-07-23 21:24:08 -0500

    Creacion de interfaces e implementación en TodoProvider

--- src/todo/context/TodoProvider.tsx
+++ src/todo/context/TodoProvider.tsx
@@ -1,4 +1,17 @@
 import { TodoContext } from "./TodoContext"
+import { TodoState } from '../interfaces/interfaces';
+
+    const INITIAL_STATE: TodoState = {
+        todoCount: 2,
+        todos: [
+            { id: 1, text: "todo 1", completed: false },
+            { id: 2, text: "todo 2", completed: false },
+            { id: 3, text: "todo 3", completed: false },
+            { id: 4, text: "todo 4", completed: false },
+        ],
+        completed: 0,
+        pending: 2,
+    }
 
 interface TodoProps {
     children: JSX.Element | JSX.Element[]
